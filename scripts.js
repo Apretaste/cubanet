59d57b4311a30434abb1dd07ec2f7e17105a0a79
kumahacker <kumahavana@gmail.com>
2019-02-23 12:08:52 -0500

    updates

---
+++ scripts.js
@@ -0,0 +1,18 @@
+function formatDate(date) {
+
+    if (typeof date != "object")
+        date = new Date(date);
+
+    var monthNames = [
+        "Enero", "Febrero", "Marzo",
+        "Abril", "Mayo", "Junio", "Julio",
+        "Agosto", "Septiembre", "Octubre",
+        "Noviembre", "Diciembre"
+    ];
+
+    var day = date.getDate();
+    var monthIndex = date.getMonth();
+    var year = date.getFullYear();
+
+    return day + ' ' + monthNames[monthIndex] + ' ' + year;
+}
